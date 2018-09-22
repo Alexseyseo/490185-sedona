6db49f5358534d1a9e7ee5c00360916c3276cc0c
Aleksey Suprun <Alexsey.seo@gmail.com>
2018-09-22 21:30:47 +0300

    Исправление замечаний по критериям № 1.2
    - Доработаы стили и скрипт для рекции на не заполненные поля формы поиска на главной странице

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-var button = document.querySelector(".search-button");
-var popup = document.querySelector(".search-popup");
-popup.classList.remove("active");
+  var button = document.querySelector(".search-button");
+  var popup = document.querySelector("form");
+  var arrival = popup.querySelector("[name=date-of-arrival]");
+  var departure = popup.querySelector("[name=date-of-departure]");
+  var adults = popup.querySelector("[name=amount-of-adults]");
+  var children = popup.querySelector("[name=amount-of-children]");
+  popup.classList.remove("active");
 
-button.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.toggle("active");
-});
+  button.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("error");
+    popup.classList.toggle("active");
+  });
+
+  popup.addEventListener("submit", function (evt) {
+      if (!arrival.value || !departure.value || !adults.value || !children.value) {
+        evt.preventDefault();
+        popup.classList.remove("error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("error");
+      }
+  });
